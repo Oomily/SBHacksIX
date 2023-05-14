4564dfe1aca3e4febe78f31b0901b564d1714e68
Rachel <racheljiang310@gmail.com>
2023-05-13 23:42:48 -0700

    merge

--- food-app/components/Dashboard.js
+++ food-app/components/Dashboard.js
@@ -1,48 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import {Text, View, CheckBox, TextInput, SafeAreaView} from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list'
+// KNhVQXTOMKnoS2oyhAFGJZdqHOOJlWUH
+const allergies = [];
+//filtering Allergies
+const filterAllergies = (nameOfItem) => {
+    for(let i = 0; i < allergies.length; i++){
+        if(nameOfItem.toLowerCase().includes(allergies[i])) return true;
+    }
+    return false;
+};
 
 export default function Dashboard() {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
     const [menu, setMenu] = useState([]);
     const [selected, setSelected] = React.useState("");
+
     info = []
     for (let i = 0; i < data.length; i++){
       info.push({key:i, value:(data[i])["name"]})
     }
-    // KNhVQXTOMKnoS2oyhAFGJZdqHOOJlWUH
+
     const getData = async () => {
-      try {
-        const response = await fetch('https://api.ucsb.edu/dining/menu/v1/2023-05-12/', {
-          method: 'GET',
-          headers: {
+        try {
+        const response = await fetch('https://api.ucsb.edu/dining/menu/v1/2023-05-18/', {
+            method: 'GET',
+            headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
             'ucsb-api-key': 'KNhVQXTOMKnoS2oyhAFGJZdqHOOJlWUH'
-          }
+            }
         });
         const json = await response.json();
         setData(json);
-        // console.log("\n", data)
-      } catch (error) {
-          console.error(error);
-      } finally {
-          setLoading(false);
-      }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
     };
-  
     const getMenu = async (selected) => {
-      let dining = "";
+        let dining = "";
       for(let i = 0; i < selected.length; i++){
         if(selected[i] == " "){
           dining += "-";
         }
         else{ dining += selected[i].toLowerCase();}
       }
-      console.log(dining);
+      //console.log(dining);
       let url = "https://api.ucsb.edu/dining/menu/v1/2023-05-18/"+dining+"/lunch";
       try {
         const response = await fetch(url, {
@@ -55,7 +62,6 @@ export default function Dashboard() {
         });
         const json = await response.json();
         setMenu(json);
-        console.log(menu);
         } catch (error) {
             console.error(error);
         } finally {
@@ -64,13 +70,18 @@ export default function Dashboard() {
     };
   
     useEffect(() => {
-      getData();}, []);
+      if(data.length == 0) {getData();}}, []);
   
+    //   Gets new menu when it updates
     useEffect(() => {
-      if(selected.length > 0){getMenu(selected);}},[]);
+      if(selected.length > 0){
+        getMenu(selected);
+        console.log("\n\nuseEffect for getting Menu\n\n");
+    }
+    else{ return;}},[]);
     
       const items = menu.map((food) => {
-        if(selected.length > 0){
+        if(selected.length > 0 && filterAllergies(food["name"].toLowerCase()) == false){
           return (
             <View>
               <Text>{food["station"]}: {food["name"]}</Text>
@@ -78,6 +89,25 @@ export default function Dashboard() {
           )
         }
       });
+
+      function Allergies(){
+        const appendAllergies = (text) => {
+            if(text.length > 0 && allergies.includes(text)==false){
+                allergies.push(text.toLowerCase());
+                if(selected.length > 0) getMenu(selected);
+                console.log("Allergies: " + allergies);
+            }
+        }
+        return (
+            <View>
+                <TextInput
+                onSubmitEditing={(value) => appendAllergies(value.nativeEvent.text)}
+                placeholder="type filter"
+                autoCapitalize='none'
+                />
+            </View>
+        )
+    };
     return(
       <View style={{paddingTop: 40}}>
         <StatusBar barStyle="light_content"/>
@@ -91,7 +121,11 @@ export default function Dashboard() {
             boxStyles={{borderRadius:0}} //override default styles
             save="value"
         />
+        <Allergies/>
         {items}
       </View>
     );
 }
+
+
+
